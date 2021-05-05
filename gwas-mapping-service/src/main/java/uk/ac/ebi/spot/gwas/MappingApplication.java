185edab6d055345854c293ec4d73d704dae6a31e
sprintell <abayomi@sprintell.org>
2021-05-05 15:24:57 +0100

    Initial project setup

---
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/MappingApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.spot.gwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MappingApplication.class, args);
+    }
+
+}
