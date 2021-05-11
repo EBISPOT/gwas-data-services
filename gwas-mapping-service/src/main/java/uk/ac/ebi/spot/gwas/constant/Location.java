763d6f99536515db585dd8961a1d74ef44fe5c87
sprintell <abayomi@sprintell.org>
2021-05-11 11:00:25 +0100

    Add feature to retrieve Variation data from ensemble api

---
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/constant/Location.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.spot.gwas.constant;
+
+public class Location {
+
+    public static final String VARIATION = "https://rest.ensembl.org/variation/homo_sapiens";
+
+
+    private Location(){
+        // Never called
+    }
+
+
+    // [https://rest.ensembl.org/overlap/region/homo_sapiens/{chromosome}:{pos_up}-{position}?feature=gene]
+
+}
