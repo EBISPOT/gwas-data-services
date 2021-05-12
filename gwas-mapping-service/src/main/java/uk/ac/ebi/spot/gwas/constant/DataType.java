12966b170d8ff132ae28ae989f026962a006d17c
sprintell <abayomi@sprintell.org>
2021-05-12 20:21:15 +0100

    Add feature to get reported genes from Ensembl with in memory caching

--- gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/constant/DataType.java
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/constant/DataType.java
@@ -2,7 +2,9 @@ package uk.ac.ebi.spot.gwas.constant;
 
 public enum DataType {
 
-    VARIATION(Location.VARIATION_FILE);
+    VARIATION(Location.VARIATION_FILE),
+
+    REPORTED_GENES(Location.REPORTED_GENES_FILE);
 
     private final String value;
 
