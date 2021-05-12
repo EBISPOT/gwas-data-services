88e05a822b3800055e86ba199b31ca7ed94cdc22
sprintell <abayomi@sprintell.org>
2021-05-12 08:13:41 +0100

    Add feature to load, cache and process ariation data from Ensembl

--- gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/service/DataLoadingService.java
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/service/DataLoadingService.java
@@ -1,15 +1,19 @@
 package uk.ac.ebi.spot.gwas.service;
 
+import org.apache.commons.collections4.ListUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.spot.gwas.constant.DataType;
 import uk.ac.ebi.spot.gwas.dto.MappingDto;
+import uk.ac.ebi.spot.gwas.dto.Variation;
 import uk.ac.ebi.spot.gwas.model.Association;
+import uk.ac.ebi.spot.gwas.util.CacheUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -22,6 +26,49 @@ public class DataLoadingService {
 
     @Autowired
     private AssociationService service;
+
+    @Autowired
+    private MappingApiService mappingApiService;
+
+    @Value("${mapping.cache}/${mapping.version}/")
+    private String cacheDir;
+
+    public Map<String, Variation> getVariation(int threadSize,
+                                               int batchSize,
+                                               List<String> snpRsIds) throws ExecutionException, InterruptedException {
+
+        long start = System.currentTimeMillis();
+        Map<String, Variation> cached = CacheUtil.variation(DataType.VARIATION, cacheDir);
+        Map<String, Variation> report = new HashMap<>();
+        int partitionSize = threadSize * batchSize;
+
+        List<String> getFromApi = new ArrayList<>();
+        for (String snpRsId : snpRsIds) {
+            Variation variation = cached.get(snpRsId.trim());
+            if (variation != null) {
+                report.putAll(Collections.singletonMap(snpRsId, variation));
+                log.info("found {}", snpRsId);
+            } else {
+                log.info("not found {}", snpRsId);
+                getFromApi.add(snpRsId);
+            }
+        }
+        log.info("Found {} in cache out of {} Remains: {}", snpRsIds.size() - getFromApi.size(), snpRsIds.size(), getFromApi.size());
+
+        for (List<String> dataPartition : ListUtils.partition(getFromApi, partitionSize)) {
+            List<CompletableFuture<Map<String, Variation>>> futureList = ListUtils.partition(dataPartition, batchSize)
+                    .stream()
+                    .map(listPart -> mappingApiService.variationPost(listPart)).collect(Collectors.toList());
+
+            CompletableFuture.allOf(futureList.toArray(new CompletableFuture[futureList.size()]));
+            for (CompletableFuture<Map<String, Variation>> future : futureList) {
+                report.putAll(future.get());
+            }
+            CacheUtil.saveToFile(DataType.VARIATION, cacheDir, report);
+        }
+        log.info("Total variation api call time {}", (System.currentTimeMillis() - start));
+        return report;
+    }
 
     public MappingDto getSnpsLinkedToLocus(int threadSize, int batchSize) throws ExecutionException, InterruptedException {
         int pageStart = 0;
