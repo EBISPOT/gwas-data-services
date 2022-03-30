49f7404b5138631819ebccd459e9baab8bf2a019
sprintell <abayomi@sprintell.org>
2022-03-30 08:19:27 +0100

    Stream Line the Command line options

---
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/cli/Cli.java
@@ -0,0 +1,74 @@
+package uk.ac.ebi.spot.gwas.cli;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.spot.gwas.common.util.CommandUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Component
+public class Cli implements CommandLineRunner {
+
+    private static final String APP_COMMAND = "java -jar gwas-mapping-service.jar";
+    private final CommandLineParser parser = new DefaultParser();
+    private final HelpFormatter help = new HelpFormatter();
+    private final Options options = CommandUtil.bindOptions();
+
+    @Autowired
+    private EnsemblRunnner ensemblRunnner;
+
+    @Override
+    public void run(String... args) throws ParseException, InterruptedException, ExecutionException, IOException {
+
+        CommandLine commandLine = parser.parse(options, args, true);
+        boolean mode = commandLine.hasOption(CommandUtil.EXEC_MODE_OPT);
+        List<String> argList = commandLine.getArgList();
+
+        if (mode) {
+            String execMode = (argList.size() > 0) ? argList.get(0) : "";
+            int threadSize = (argList.size() > 1) ? Integer.parseInt(argList.get(1)) : 40;
+            this.menuDecision(execMode, threadSize);
+        } else {
+            help.printHelp(130, APP_COMMAND,  "", options,"");
+            System.exit(1);
+        }
+    }
+
+
+    public void menuDecision(String executionMode, int threadSize) throws InterruptedException, ExecutionException, IOException {
+        switch (executionMode) {
+            case "map-all-snp":
+                log.info("Mapping -r {}", executionMode);
+                ensemblRunnner.mapAllAssociations(executionMode);
+                System.exit(1);
+                break;
+            case "map-some-snp":
+                log.info("Mapping -m {}", executionMode);
+                ensemblRunnner.mapSomeAssociations(executionMode);
+                System.exit(1);
+                break;
+            case "cache-ensembl-data":
+                ensemblRunnner.runCache(threadSize);
+                System.exit(1);
+                break;
+            case "server-mode":
+                log.info("Application executed successfully, running in server mode!");
+                break;
+            default:
+                log.info("The mode value {} is not recognized", executionMode);
+                help.printHelp(APP_COMMAND, options, true);
+                System.exit(1);
+
+        }
+    }
+}
+
+
+// -m automatic_mapping_process 40
--- gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/cli/ScheduledTasks.java
+++ gwas-mapping-service/src/main/java/uk/ac/ebi/spot/gwas/cli/ScheduledTasks.java
@@ -1,11 +1,10 @@
-package uk.ac.ebi.spot.gwas;
+package uk.ac.ebi.spot.gwas.cli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import uk.ac.ebi.spot.gwas.service.mapping.EnsemblRunnner;
 
 import java.util.concurrent.ExecutionException;
 
@@ -16,7 +15,7 @@ public class ScheduledTasks {
     private EnsemblRunnner ensemblRunnner;
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    @Scheduled(fixedDelay = 18000000)
+    //@Scheduled(fixedDelay = 18000000)
     public void scheduleTaskWithFixedDelay() throws ExecutionException, InterruptedException {
         log.info("Fixed Delay Task :: Starting Mapping Pipeline");
         String performer = "automatic_mapping_process";
