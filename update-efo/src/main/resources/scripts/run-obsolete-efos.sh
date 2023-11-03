d578b8e216cf7399bf72f446e8ac2e5d80ceb52f
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2023-11-03 12:36:12 +0000

    goci-1134 externalizing DB credentials

--- update-efo/src/main/resources/scripts/run-obsolete-efos.sh
+++ update-efo/src/main/resources/scripts/run-obsolete-efos.sh
@@ -6,6 +6,6 @@ proxy_settings="-Dhttp.proxyHost=www-proxy.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhtt
 configlocation=/hps/software/users/parkinso/spot/gwas/prod/sw/mapping-pipeline/config
 source ${configlocation}/db-env
 
-java -DentityExpansionLimit=100000000  -Dspring.profiles.active=${profile} -Dspring.data.mongodb.username=${DB_USER} Dspring.data.mongodb.password=${DB_PWD}  $proxy_settings -Xms4096m -Xmx4096m -jar $base/update-efo.jar
+java -DentityExpansionLimit=100000000  -Dspring.profiles.active=${profile} -Dspring.data.mongodb.username=${MONGO_DB_USER} Dspring.data.mongodb.password=${MONGO_DB_PWD}  $proxy_settings -Xms4096m -Xmx4096m -jar $base/update-efo.jar
 
 exit $?
