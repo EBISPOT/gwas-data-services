9ce78b113787d7273e1c3ac71eea6df0fd1d6043
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2023-11-28 13:32:42 +0000

    goci-1180 SLurm migration of mapping pipeline adding script location in script

--- gwas-mapping-pipeline/src/main/resources/scripts/run-slurm-mapper.sh
+++ gwas-mapping-pipeline/src/main/resources/scripts/run-slurm-mapper.sh
@@ -4,6 +4,7 @@
 #SBATCH --mem=4G
 
 base=${0%/*}/;
+scriptLocation=/hps/software/users/parkinso/spot/gwas/prod/sw/mapping-pipeline
 hashdir=${1}
 asscnIds=${2}
 executorPool=${3}
@@ -17,5 +18,5 @@ echo "Bsub log dir is: ${logslocation}/${executorPool}/${hashdir}"
 
 echo "sbatch -t 1:00:00 -mem=4G -o ${logslocation}/${executorPool}/${hashdir}/output.log -e ${logslocation}/${executorPool}/${hashdir}/error.log -J  \"gwas-mapper\" --partition=short ${base}/map-association.sh -m map-asscn-ids 40 ${asscnIds} ${logslocation}/${executorPool}/${hashdir} "
 
-${base}/map-association.sh -m map-asscn-ids 40 ${asscnIds} ${logslocation}/${executorPool}/${hashdir}
+${scriptLocation}/map-association.sh -m map-asscn-ids 40 ${asscnIds} ${logslocation}/${executorPool}/${hashdir}
 exit $?
