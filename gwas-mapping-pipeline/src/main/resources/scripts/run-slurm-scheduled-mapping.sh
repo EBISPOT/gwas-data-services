329e17f79766489c3aef711b1c70bcd3a27fc5c3
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2024-02-16 17:29:53 +0000

    goci-1227 Full Ensembl remap to 111 version

--- gwas-mapping-pipeline/src/main/resources/scripts/run-slurm-scheduled-mapping.sh
+++ gwas-mapping-pipeline/src/main/resources/scripts/run-slurm-scheduled-mapping.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #SBATCH -t 04:00:00
-
 #SBATCH --mem=4G
-#SBATCH --output=/hps/nobackup/parkinso/spot/gwas/logs/mapping-pipeline/logs/bsub/mapping-output.log
-#SBATCH --error=/hps/nobackup/parkinso/spot/gwas/logs/mapping-pipeline/logs/bsub/mapping-error.log
 base=${0%/*}/;
 echo "base is ${base}"
 /hps/software/users/parkinso/spot/gwas/prod/sw/mapping-pipeline/mapping-pipeline.sh schedule
