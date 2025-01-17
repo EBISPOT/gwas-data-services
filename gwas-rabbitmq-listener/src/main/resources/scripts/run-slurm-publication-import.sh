1e0256199066ea54d64dc385dba6acd9cb1acfb9
Sajo <sajo@ebi.ac.uk>
2025-01-17 18:02:40 +0000

    goci-1461-cleanup-homes-dir adding output to sbatch scripts

--- gwas-rabbitmq-listener/src/main/resources/scripts/run-slurm-publication-import.sh
+++ gwas-rabbitmq-listener/src/main/resources/scripts/run-slurm-publication-import.sh
@@ -2,6 +2,8 @@
 #SBATCH -t 48:00:00
 #SBATCH --mem=4G
 #SBATCH --job-name=publication-import
+#SBATCH -o /hps/nobackup/parkinso/spot/gwas/logs/sbatch/prod/rabbitmq-start.out
+#SBATCH -e /hps/nobackup/parkinso/spot/gwas/logs/sbatch/prod/rabbitmq-start.err
 base=${0%/*}/;
 echo "base is ${base}"
 /hps/software/users/parkinso/spot/gwas/prod/sw/gwas-rabbitmq-listener/publication-import.sh
