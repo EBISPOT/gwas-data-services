421bc0e5197df862789c191ca9c17e90bd8ed12c
Sajo <sajo@ebi.ac.uk>
2025-04-02 22:01:32 +0100

    data-services-95-parent-trait-mapping handling partition for large nunmber of EFO children

--- gwas-association-parent-trait-mapper/src/main/java/uk/ac/ebi/spot/gwas/Application.java
+++ gwas-association-parent-trait-mapper/src/main/java/uk/ac/ebi/spot/gwas/Application.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = "uk.ac.ebi.spot.gwas")
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
