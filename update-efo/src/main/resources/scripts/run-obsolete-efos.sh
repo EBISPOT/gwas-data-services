551e9b01f7a1bad3fd4448961a40edbb73a715fa
ala <63405719+ala-ebi@users.noreply.github.com>
2026-04-11 10:53:44 +0100

    update run-obsolete-efos.sh: adjust log file paths and jar location variable

--- update-efo/src/main/resources/scripts/run-obsolete-efos.sh
+++ update-efo/src/main/resources/scripts/run-obsolete-efos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH -t 02:00:00
 #SBATCH --mem=4G
-#SBATCH -o /hps/nobackup/parkinso/spot/gwas/logs/sbatch/prod/obsolete_efo.out
-#SBATCH -e /hps/nobackup/parkinso/spot/gwas/logs/sbatch/prod/obsolete_efo.err
+#SBATCH -o /hps/nobackup/parkinso/spot/gwas/logs/obsolete-efo/slurm-obsolete-efo.out
+#SBATCH -e /hps/nobackup/parkinso/spot/gwas/logs/obsolete-efo/slurm-obsolete-efo.err
 
 profile=${1}
 proxy_settings="-Dhttp.proxyHost=www-proxy.ebi.ac.uk -Dhttp.proxyPort=3128 -Dhttp.nonProxyHosts=*.ebi.ac.uk -Dftp.proxyHost=www-proxy.ebi.ac.uk -Dftp.proxyPort=3128 -Dftp.nonProxyHosts=*.ebi.ac.uk -DproxyHost=www-proxy.ebi.ac.uk -DproxyPort=3128 -DproxySet=true";
 configlocation=/hps/software/users/parkinso/spot/gwas/prod/sw/mapping-pipeline/config
 source ${configlocation}/db-env
+jarLocation=/hps/software/users/parkinso/spot/gwas/prod/sw/mapping-pipeline
 
 java -DentityExpansionLimit=100000000 \
   -Dspring.profiles.active=${profile} \
@@ -17,4 +18,4 @@ java -DentityExpansionLimit=100000000 \
   -Dspring.datasource.username=${DB_USER} \
   -Dspring.datasource.password=${DB_PWD} \
   -Dspring.datasource.driver-class-name=${DB_DRIVER} \
-  $proxy_settings -Xms4096m -Xmx4096m -jar $base/update-efo.jar
+  $proxy_settings -Xms4096m -Xmx4096m -jar ${jarLocation}/update-efo.jar
